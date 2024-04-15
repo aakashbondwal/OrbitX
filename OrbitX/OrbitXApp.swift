39871bea3462313d1644a746a68a9b897e19309c
Aakash Bondwal <94138250+aakashbondwal@users.noreply.github.com>
2024-04-16 05:01:53 +0530

    Initial Commit

---
+++ OrbitX/OrbitXApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrbitXApp.swift
+//  OrbitX
+//
+//  Created by Aakash  Bondwal  on 16/04/24.
+//
+
+import SwiftUI
+
+@main
+struct OrbitXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
